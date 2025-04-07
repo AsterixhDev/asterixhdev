79533c28c405382477e270009e95c144079f3776
asterixh <112446367+AsterixhDev@users.noreply.github.com>
2025-04-07 19:32:36 +0000

    Add Skeleton, Separator, and Tooltip components; implement mobile sidebar and update layout structure

--- app/(PAGES)/layout.tsx
+++ app/(PAGES)/layout.tsx
@@ -1,5 +1,6 @@
 import Footer from '@/components/Sections/Majors/Footer'
 import Header from '@/components/Sections/Majors/Header'
+import { SideBarMobile } from '@/components/Sections/Majors/SideBarMobile'
 import React from 'react'
 
 type Props = {
@@ -9,7 +10,9 @@ type Props = {
 export default function layout({children}: Props) {
   return (
     <>
-    <div className="size-full">
+    <div className="size-full h-screen">
+    <SideBarMobile/>
+
     <main className='max-h-full relative isolate overflow-y-scroll'>
     <Header/>
     {children}
