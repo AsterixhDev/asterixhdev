1058fe2e74fc5512ea18f0fa302a8eaf051f9ef8
asterixh <112446367+AsterixhDev@users.noreply.github.com>
2025-04-12 22:31:47 +0000

    feat: enhance ProjectCard usage by adding index prop for better tracking in ProjectGrid and Projects components

--- components/Sections/Admin/portfolio/ProjectComponent.tsx
+++ components/Sections/Admin/portfolio/ProjectComponent.tsx
@@ -7,8 +7,8 @@ import { Project } from "@/lib/types";
 export function ProjectGrid({ projects }: { projects: Project[] }) {
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-      {projects.map((project) => (
-        <ProjectCard showEdit={true} key={`${project._id}`} project={project} />
+      {projects.map((project, index) => (
+        <ProjectCard showEdit={true} key={`${project._id}`}  index={index+1} project={project} />
       ))}
     </div>
   );
